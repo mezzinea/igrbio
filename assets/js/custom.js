7ebfc9e92aa4641dd63db7166f6e2369be920b10
abdellatif <Abdellatif.Mezzine@easyfairs.com>
2025-09-28 18:22:36 +0100

    global views

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,28 @@ document.querySelectorAll('.dropdown-item').forEach(item => {
     document.getElementById('selected-flag').alt = langText;
     });
 });
+
+
+// Function to load HTML into an element
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading HTML:', error);
+        });
+}
+
+// Load the header and footer when the page is ready
+document.addEventListener('DOMContentLoaded', function() {
+    loadHTML('header', 'global/header.html');
+    loadHTML('footer', 'global/footer.html');
+    loadHTML('head', 'global/head.html');
+});
